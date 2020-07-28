4652d357e51c19dc48b02e3504bb49158c0fdfc7
darrylgit <darrylsmithup@gmail.com>
2020-07-28 11:30:11 -0400

    Initial commit

--- client/src/pages/checkout/checkout.component.jsx
+++ client/src/pages/checkout/checkout.component.jsx
@@ -3,10 +3,10 @@ import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
 import CheckoutItem from '../../components/checkout-item/checkout-item.component';
-import StripeCheckoutButton from '../../components/stripe-button/stripe-button.component';
+
 import {
   selectCartItems,
-  selectCartTotal
+  selectCartTotal,
 } from '../../redux/cart/cart.selectors';
 
 import './checkout.styles.scss';
@@ -30,24 +30,18 @@ const CheckoutPage = ({ cartItems, total }) => (
         <span>Remove</span>
       </div>
     </div>
-    {cartItems.map(item => (
+    {cartItems.map((item) => (
       <CheckoutItem key={item.id} cartItem={item} />
     ))}
     <div className="total">
       <span>TOTAL: ${total}</span>
     </div>
-    <div className="test-warning">
-      *Please use the following test credit card for payments*
-      <br />
-      4242-4242-4242-4242
-    </div>
-    <StripeCheckoutButton price={total} />
   </div>
 );
 
 const mapStateToProps = createStructuredSelector({
   cartItems: selectCartItems,
-  total: selectCartTotal
+  total: selectCartTotal,
 });
 
 export default connect(mapStateToProps)(CheckoutPage);
